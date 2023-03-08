1e9df6df28d859c78baec56b0366afb89b192877
userdev01rgithub <47008929+userdev01rgithub@users.noreply.github.com>
2023-03-08 10:31:52 +0300

    mini fix

--- config/web.php
+++ config/web.php
@@ -42,14 +42,16 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+        
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '' => 'site/index',                                
+                '<controller:\w+>/<action:\w+>/' => '<controller>/<action>',
             ],
         ],
-        */
+        
     ],
     'params' => $params,
 ];
